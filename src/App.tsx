16ec3ee7fbb84923fa88853c2d720713820470a1
MaksymTertychnyj <maka12008@ukr.net>
2022-05-14 14:28:07 +0300

    added library

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import "./App.css";
+import { url } from "inspector";
 import Login from "./components/login/Login";
+import Providers from "./components/navigation/Providers";
 
 const App = () => {
   return (
-    <div>
-      Start
-      <Login />
+    <div
+      style={{
+        flex: 1,
+        backgroundImage: `url(${require("./public/images/background.png")})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: window.innerHeight,
+      }}
+    >
+      <Providers />
     </div>
   );
 };
