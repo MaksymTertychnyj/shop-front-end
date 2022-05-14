a27c2a048e50e3a842a4aba40af684934a652dad
MaksymTertychnyj <maka12008@ukr.net>
2022-05-14 14:28:39 +0300

    added Providers

---
+++ src/components/navigation/Providers.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginProvider from "../login-provider/LoginProvider";
+
+const Providers = () => {
+  return <LoginProvider></LoginProvider>;
+};
+
+export default Providers;
