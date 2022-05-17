4a194591aad32d46046f00ece29333e74ad5f270
MaksymTertychnyj <maka12008@ukr.net>
2022-05-17 12:42:39 +0300

    added pages of employees

---
+++ src/components/navigation/AppCard/app-card-tabs/app-card-activities/employees/Employees.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EmployeesStyles from "./EmployeesStyles";
+
+const Employee = () => {
+  return <div className={EmployeesStyles.container}>Employees page</div>;
+};
+
+export default Employee;
---
+++ src/components/navigation/AppCard/app-card-tabs/app-card-activities/employees/EmployeesStyles.tsx
@@ -0,0 +1,10 @@
+import { style } from "typestyle";
+
+const EmployeesStyles = {
+  container: style({
+    width: 800,
+    height: 400,
+  }),
+};
+
+export default EmployeesStyles;
