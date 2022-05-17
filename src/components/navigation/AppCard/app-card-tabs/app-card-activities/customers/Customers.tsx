78af41379dfb284615ff8321a816ceed0681f6b4
MaksymTertychnyj <maka12008@ukr.net>
2022-05-17 12:42:55 +0300

    added pages of customers

---
+++ src/components/navigation/AppCard/app-card-tabs/app-card-activities/customers/Customers.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CustomersStyles from "./CustomrsStyles";
+
+const Customers = () => {
+  return <div className={CustomersStyles.container}>Customers page</div>;
+};
+
+export default Customers;
---
+++ src/components/navigation/AppCard/app-card-tabs/app-card-activities/customers/CustomrsStyles.tsx
@@ -0,0 +1,10 @@
+import { style } from "typestyle";
+
+const CustomersStyles = {
+  container: style({
+    width: 800,
+    height: 400,
+  }),
+};
+
+export default CustomersStyles;
